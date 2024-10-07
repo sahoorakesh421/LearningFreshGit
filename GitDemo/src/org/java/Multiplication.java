32b7e3fb069250ac40a091e6c3ded3e05a03ef61
Rakesh Sah <sahoorakesh421@gmail.com>
2024-10-07 23:37:28 +0530

    adding abcd comment for testttt

--- GitDemo/src/org/java/Multiplication.java
+++ GitDemo/src/org/java/Multiplication.java
@@ -16,7 +16,9 @@ public class Multiplication {
         double num2 = scanner.nextDouble();
         
         // Calculate the product
-        double product = num1 * num2;
+        double product = num1 * num2;//test567890
+        
+        //testtttt
         
         // Display the product
         System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
